1ca92edca8c0277bd88703ac0522fe67715b9781
Alexey Trilis <aatrilis2000@gmail.com>
2025-03-17 23:14:11 +0400

    bibtex template

--- short_presentation/build.sh
+++ short_presentation/build.sh
@@ -2,4 +2,4 @@ xelatex presentation-short.tex
 biber presentation-short
 xelatex presentation-short.tex > /dev/null
 xelatex presentation-short.tex > /dev/null
-xdg-open presentation-short.pdf
+open presentation-short.pdf
--- text/build.sh
+++ text/build.sh
@@ -2,4 +2,4 @@ xelatex --shell-escape diploma.tex
 bibtex diploma.aux
 xelatex --shell-escape diploma.tex > /dev/null
 xelatex --shell-escape diploma.tex > /dev/null
-xdg-open diploma.pdf
+open diploma.pdf
